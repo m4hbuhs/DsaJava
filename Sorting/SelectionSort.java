9a801e70eb0dac6c7dd292a8f9cdc1a842f2dcaf
m4hbuhs <itsshubham206@gmail.com>
2026-07-20 18:51:03 +0530

    updated gitignore file

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,13 @@
+class SelectionSort {
+    // select minimum and swap
+    static void sort(int[] arr,int min,int max){
+        for(int i = 0;i<arr.length;i++){
+            int
+
+        }
+
+    }
+
+    
+} 
+
